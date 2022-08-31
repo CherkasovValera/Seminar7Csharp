5f7c454733eee4b6dc7c75d523ee4c195bced2f4
Valeriy <cherkasov77@mail.ru>
2022-08-31 20:44:09 +0400

    всезадачиСеминар7

--- Program.cs
+++ Program.cs
@@ -46,91 +46,120 @@
 //
 
 
-Console.Clear();
-int Prompt(string message)
-{
-    System.Console.Write(message);
-    int result = Convert.ToInt32(Console.ReadLine());
-    return result;
-}
-int[,] FillArray(int numLine, int numColumns, int maxRand, int minRand)
-{
-    int[,] matrix = new int[numLine, numColumns];
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = new Random().Next(minRand, maxRand);
-        }
-    }
-    return matrix;
-}
-void PrintArray(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i, j],6}");
-        }
-        Console.WriteLine();
-    }
-}
-int numLine = Prompt("Введите количество строк массива ");
-int numColumns = Prompt("Введите количество столбцов массива ");
-int maxRand = Prompt("Введите верхнюю границу диапазона ");
-int minRand = Prompt("Введите нижнюю границу диапазона ");
-int[,] matrix = FillArray(numLine, numColumns, maxRand, minRand);
-PrintArray(matrix);
-
-int stroka = 0;
-int stolbec = 0;
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-    }
-    stolbec++;
-    stroka++;
-}
-Console.WriteLine("Введите строку: ");
-int horizont = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите столбец: ");
-int vertikal = Convert.ToInt32(Console.ReadLine());
-if (horizont <= stroka && vertikal <= stolbec)
-{
-    Console.WriteLine($"Выбранный элемент: {matrix[horizont - 1, vertikal - 1]}");
-}
-else
-{
-    Console.WriteLine("В данном массиве не такого элемента!!!");
-}
-
-
-// int[,] massiv = new int[4,4];
-// Random sluchay = new Random();
-// int counterLine = 0;
-// int counterColomn = 0;
-// for (int i = 0; i < massiv.GetLength(0); i++)
+// Console.Clear();
+// int Prompt(string message)
 // {
-//     for (int j = 0; j < massiv.GetLength(1); j++)
-//     {
-//         massiv[i,j] = sluchay.Next(1, 9);
-//         Console.Write($"{massiv[i, j]}\t");
-//     }
-//     Console.WriteLine();
-//        counterColomn++;
-//     counterLine++;
+//     System.Console.Write(message);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
 // }
-// Console.WriteLine("Введите строку массива: ");
-// int line = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите столбец массива: ");
-// int column = Convert.ToInt32(Console.ReadLine());
-// if (line<=counterLine &&column<=counterColomn)
+// int[,] FillArray(int numLine, int numColumns, int maxRand, int minRand)
 // {
-//     Console.WriteLine($"Значение выбранного элемента: {massiv[line-1,column-1]}");
+//     int[,] matrix = new int[numLine, numColumns];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(minRand, maxRand);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j],6}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int numLine = Prompt("Введите количество строк массива ");
+// int numColumns = Prompt("Введите количество столбцов массива ");
+// int maxRand = Prompt("Введите верхнюю границу диапазона ");
+// int minRand = Prompt("Введите нижнюю границу диапазона ");
+// int[,] matrix = FillArray(numLine, numColumns, maxRand, minRand);
+// PrintArray(matrix);
+
+// int stroka = 0;
+// int stolbec = 0;
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//     }
+//     stolbec++;
+//     stroka++;
+// }
+// Console.WriteLine("Введите строку: ");
+// int horizont = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите столбец: ");
+// int vertikal = Convert.ToInt32(Console.ReadLine());
+// if (horizont <= stroka && vertikal <= stolbec)
+// {
+//     Console.WriteLine($"Выбранный элемент: {matrix[horizont - 1, vertikal - 1]}");
 // }
 // else
 // {
-//     Console.WriteLine("Такого элемента нет, вы вышли за пределы массива.");
+//     Console.WriteLine("В данном массиве не такого элемента!!!");
 // }
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Console.Clear();
+// int Prompt(string message)
+// {
+//     System.Console.Write(message);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
+// }
+// int[,] FillArray(int numLine, int numColumns, int maxRand, int minRand)
+// {
+//     int[,] matrix = new int[numLine, numColumns];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(minRand, maxRand);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"\t{matrix[i, j],6}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int numLine = Prompt("Введите количество строк массива ");
+// int numColumns = Prompt("Введите количество столбцов массива ");
+// int maxRand = Prompt("Введите верхнюю границу диапазона ");
+// int minRand = Prompt("Введите нижнюю границу диапазона ");
+// int[,] matrix = FillArray(numLine, numColumns, maxRand, minRand);
+// PrintArray(matrix);
+
+//     Console.WriteLine($"Среднее арифметическое по столбцу:");
+//     int summa = 0;
+//     double sredarif = 0;
+//     for (int i = 0; i < matrix.GetLength(1); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(0); j++)
+//         {
+//             summa += matrix[j, i];
+//         }
+//         sredarif = (double)summa / matrix.GetLength(0);
+//         Console.Write($"\t  {sredarif:f2}");
+//         summa = 0;
+//     }
+
