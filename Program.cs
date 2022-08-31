14f8e03d39486b1ab740ae0e6dad32dff9aa0722
Valeriy <cherkasov77@mail.ru>
2022-08-31 16:58:48 +0400

    2ЗадачаправкаСеминар7

--- Program.cs
+++ Program.cs
@@ -46,62 +46,91 @@
 //
 
 
-// Console.Clear();
-// int Prompt(string message)
-// {
-//     System.Console.Write(message);
-//     int result = Convert.ToInt32(Console.ReadLine());
-//     return result;
-// }
-// int[,] FillArray(int numLine, int numColumns, int maxRand, int minRand)
-// {
-//     int[,] matrix = new int[numLine, numColumns];
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             matrix[i, j] = new Random().Next(minRand, maxRand);
-//         }
-//     }
-//     return matrix;
-// }
-// void PrintArray(int[,] matrix)
-// {
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             Console.Write($"{matrix[i, j],6}");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-// int numLine = Prompt("Введите количество строк массива ");
-// int numColumns = Prompt("Введите количество столбцов массива ");
-// int maxRand = Prompt("Введите верхнюю границу диапазона ");
-// int minRand = Prompt("Введите нижнюю границу диапазона ");
-// int[,] matrix = FillArray(numLine, numColumns, maxRand, minRand);
-// PrintArray(matrix);
+Console.Clear();
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int[,] FillArray(int numLine, int numColumns, int maxRand, int minRand)
+{
+    int[,] matrix = new int[numLine, numColumns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(minRand, maxRand);
+        }
+    }
+    return matrix;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
+int numLine = Prompt("Введите количество строк массива ");
+int numColumns = Prompt("Введите количество столбцов массива ");
+int maxRand = Prompt("Введите верхнюю границу диапазона ");
+int minRand = Prompt("Введите нижнюю границу диапазона ");
+int[,] matrix = FillArray(numLine, numColumns, maxRand, minRand);
+PrintArray(matrix);
 
-// int stroka = 0;
-// int stolbec = 0;
-// for (int i = 0; i < matrix.GetLength(0); i++)
+int stroka = 0;
+int stolbec = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+    }
+    stolbec++;
+    stroka++;
+}
+Console.WriteLine("Введите строку: ");
+int horizont = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите столбец: ");
+int vertikal = Convert.ToInt32(Console.ReadLine());
+if (horizont <= stroka && vertikal <= stolbec)
+{
+    Console.WriteLine($"Выбранный элемент: {matrix[horizont - 1, vertikal - 1]}");
+}
+else
+{
+    Console.WriteLine("В данном массиве не такого элемента!!!");
+}
+
+
+// int[,] massiv = new int[4,4];
+// Random sluchay = new Random();
+// int counterLine = 0;
+// int counterColomn = 0;
+// for (int i = 0; i < massiv.GetLength(0); i++)
 // {
-//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     for (int j = 0; j < massiv.GetLength(1); j++)
 //     {
-//         stolbec++;
+//         massiv[i,j] = sluchay.Next(1, 9);
+//         Console.Write($"{massiv[i, j]}\t");
 //     }
-//     stroka++;
+//     Console.WriteLine();
+//        counterColomn++;
+//     counterLine++;
 // }
-// Console.WriteLine("Введите строку: ");
+// Console.WriteLine("Введите строку массива: ");
 // int line = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите столбец: ");
+// Console.WriteLine("Введите столбец массива: ");
 // int column = Convert.ToInt32(Console.ReadLine());
-// if (line <= stroka && column <= stolbec)
+// if (line<=counterLine &&column<=counterColomn)
 // {
-//     Console.WriteLine($"Выбранный элемент: {matrix[line - 1, column - 1]}");
+//     Console.WriteLine($"Значение выбранного элемента: {massiv[line-1,column-1]}");
 // }
 // else
 // {
-//     Console.WriteLine("В данном массиве не такого элемента!!!");
+//     Console.WriteLine("Такого элемента нет, вы вышли за пределы массива.");
 // }
